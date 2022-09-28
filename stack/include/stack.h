c66fb6420890d19f4981df2ff412bdef4674ec48
Denis <denis.boboruchin@gmail.com>
2022-09-28 23:01:31 +0300

    stack started

---
+++ stack/include/stack.h
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <cstring>
+
+namespace my_containers
+{
+
+const size_t MIN_CAPACITY = 10;
+const size_t BIG_CAPACITY = 500;
+
+const double CAPACITY_FACTOR = 1.4;
+const int CAPACITY_ADDED = 100;
+
+template <typename T> class stack 
+{
+public:
+    stack (size_t capacity): capacity_ {capacity}
+    {
+        data_ = new T[capacity];
+    }
+
+    stack (stack& other)
+    {
+        capacity_ = other.capacity_;
+        size_ = other.size_;
+    
+        data_ = new T[other.capacity_];
+        memcpy (data_, other.data_, sizeof (T));
+    }
+
+    ~stack ()
+    {
+        delete [] data_;
+    }
+
+    void push (const T& elem)
+    {
+        this->check_size_ ();
+
+        size_++;
+        data_[size_] = elem;
+    }
+
+    void pop ()
+    {
+        this->check_size_ ();
+
+        size_--;
+    }
+
+    T& top () const
+    {
+        return data_[size_];
+    }
+
+    bool empty () const
+    {
+        return size_ == 0;
+    }
+
+    size_t size () const
+    {
+        return size_;
+    }       
+
+private:
+    void check_size_ ()
+    {
+        if (size_ == capacity_)
+        {
+            this->resize_up_ ();
+        }
+
+        else if (size_ < size_t (capacity_ / 2))
+        {
+            if (size_ > 2 * MIN_CAPACITY)
+                //this->resize_down_ ();
+            {}
+        }
+    }
+
+    void resize_up_ ()
+    {
+        if (capacity_ < size_t (BIG_CAPACITY))
+        {
+            T* new_data = new T[int (capacity_ * CAPACITY_FACTOR)];   
+
+            memcpy (new_data, data_, sizeof (T));
+            delete [] data_;
+
+            data_ = new_data;
+        }
+
+        else
+        {
+            T* new_data = new T[capacity_ + size_t (CAPACITY_ADDED)];
+
+            memcpy (new_data, data_, sizeof (T));   
+            delete [] data_;
+
+            data_ = new_data;
+        }
+    }
+
+    size_t capacity_ = 0;
+    size_t size_ = 0;
+
+    T* data_ = nullptr;
+};
+
+}
---
+++ stack/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "stack.h"
+
+int main ()
+{
+    my_containers::stack<int> stack (10);
+
+    stack.push (3);
+    stack.push (2);
+    stack.pop ();
+
+    stack.empty ();
+    std::cout << stack.top () << '\n';
+    std::cout << "size: " << stack.size () << '\n';
+
+    my_containers::stack<int> stack2 = stack;
+    
+    stack2.push (5);
+    std::cout << stack2.top () << "\nsize: " << stack2.size () << '\n';
+
+    return 0;
+}
