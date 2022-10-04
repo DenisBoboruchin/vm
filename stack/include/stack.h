0bf13086283c12a79bc520d215f7605a0591271a
Denis <denis.boboruchin@gmail.com>
2022-10-04 18:04:58 +0300

    replace methods from the class stack

--- stack/include/stack.h
+++ stack/include/stack.h
@@ -10,119 +10,31 @@ const size_t MIN_CAPACITY = 128;
 const double CAPACITY_FACTOR = 2;
 const int RATIO_TO_RESIZE_DOWN = 4;
 
+
 template <typename T> class stack 
 {
 public:
-    stack (size_t capacity = MIN_CAPACITY): capacity_ {capacity}
-    {
-        data_ = new T[capacity];
-    }
+    stack (size_t capacity = MIN_CAPACITY);
+    stack (const stack& other);
+    stack (stack&& other);
 
-    stack (const stack& other)
-    {
-        capacity_ = other.capacity_;
-        size_ = other.size_;
+    ~stack ();
+    
+    stack& operator= (const stack& other);
+    stack& operator= (stack&& other);
 
-        data_ = new T[capacity_];
-        memcpy (data_, other.data_, sizeof (T));
-    }
+    void push (const T& elem);
+    void pop ();
+    T& top () const &;
 
-    stack (stack&& other)
-    {
-        *this = std::move(other);
-    }
-
-    ~stack ()
-    {
-        delete [] data_;
-    }
-
-    stack& operator= (const stack& other) 
-    {
-        capacity_ = other.capacity_;
-        size_ = other.size_;
-        
-        delete [] data_;
-        data_ = new T[capacity_];
-        memcpy (data_, other.data_, sizeof (T));
-
-        return *this;
-    }
-
-    stack& operator= (stack&& other)
-    {
-        if (this != &other)
-        {
-            capacity_ = other.capacity_;
-            size_ = other.size_;
-            std::swap(data_, other.data_);
-        }
-
-        return *this;
-    }
-
-    void push (const T& elem)
-    {
-        this->check_size_ ();
-
-        data_[size_] = elem;
-        size_++;
-    }
-
-    void pop ()
-    {
-        this->check_size_ ();
-
-        size_--;
-    }
-
-    T& top () const &
-    {
-        return data_[size_];
-    }
-
-    bool empty () const
-    {
-        return size_;
-    }
-
-    size_t size () const
-    {
-        return size_;
-    }      
+    bool empty () const;
+    size_t size () const;     
 
 private:
-    void check_size_ ()
-    {
-        if (size_ == capacity_)
-            this->resize_up_ ();
-
-        else if (size_ < size_t (capacity_ / RATIO_TO_RESIZE_DOWN))
-            this->resize_down_ ();
-    }
-
-    void resize_up_ ()
-    {
-        T* new_data = new T[int (capacity_ * CAPACITY_FACTOR)];   
-
-        memcpy (new_data, data_, sizeof (T));
-        delete [] data_;
-
-        data_ = new_data;
-    }
-
-    void resize_down_ ()
-    {
-        if (size_ > 2 * MIN_CAPACITY)
-        {
-            T* new_data = new T[int (capacity_ / CAPACITY_FACTOR)];
-
-            memcpy (new_data, data_, sizeof (T));
-            delete [] data_;
-
-            data_ = new_data;
-        }   
-    }
+    void check_size_ ();
+    
+    void resize_up_ ();
+    void resize_down_ ();
 
 private:
     size_t capacity_ = 0;
@@ -130,5 +42,116 @@ private:
 
     T* data_ = nullptr;
 };
+
+
+template <typename T> stack<T>::stack (size_t capacity): capacity_ {capacity}
+{
+    data_ = new T[capacity];
+}
+
+template <typename T> stack<T>::stack (const stack& other)
+{
+	capacity_ = other.capacity_;
+	size_ = other.size_;
+
+	data_ = new T[capacity_];
+	memcpy (data_, other.data_, sizeof (T));
+}
+
+template <typename T> stack<T>::stack (stack&& other)
+{
+    *this = std::move(other);
+}
+
+template <typename T> stack<T>::~stack ()
+{
+    delete [] data_;
+}
+
+template <typename T> stack<T>& stack<T>::operator= (const stack& other) 
+{
+    capacity_ = other.capacity_;
+    size_ = other.size_;
+    
+    delete [] data_;
+    data_ = new T[capacity_];
+    memcpy (data_, other.data_, sizeof (T));
+
+    return *this;
+}
+
+template <typename T> stack<T>& stack<T>::operator= (stack&& other)
+{
+    if (this != &other)
+    {
+        capacity_ = other.capacity_;
+        size_ = other.size_;
+        std::swap(data_, other.data_);
+    }
+
+    return *this;
+}
+
+template <typename T> void stack<T>::push (const T& elem)
+{
+    this->check_size_ ();
+
+    data_[size_] = elem;
+    size_++;
+}
+
+template <typename T> void stack<T>::pop ()
+{
+    this->check_size_ ();
+
+    size_--;
+}
+
+template <typename T> T& stack<T>::top () const &
+{
+	return data_[size_];
+}
+
+template <typename T> bool stack<T>::empty () const
+{
+    return size_;
+}
+
+template <typename T> size_t stack<T>::size () const
+{
+    return size_;
+}      
+
+template <typename T> void stack<T>::check_size_ ()
+{
+    if (size_ == capacity_)
+        this->resize_up_ ();
+
+    else if (size_ < size_t (capacity_ / RATIO_TO_RESIZE_DOWN))
+        this->resize_down_ ();
+}
+
+template <typename T> void stack<T>::resize_up_ ()
+{
+    T* new_data = new T[int (capacity_ * CAPACITY_FACTOR)];   
+
+    memcpy (new_data, data_, sizeof (T));
+    delete [] data_;
+
+    data_ = new_data;
+}
+
+template <typename T> void stack<T>::resize_down_ ()
+{
+    if (size_ > 2 * MIN_CAPACITY)
+    {
+        T* new_data = new T[int (capacity_ / CAPACITY_FACTOR)];
+
+        memcpy (new_data, data_, sizeof (T));
+        delete [] data_;
+
+        data_ = new_data;
+    }   
+}
 
 }
