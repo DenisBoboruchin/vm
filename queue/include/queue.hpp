782ae593c8b3f5ed72e3c0086b5d2fbd0609bf1b
Denis <denis.boboruchin@gmail.com>
2022-11-24 18:05:26 +0300

    add pop, size, empty and tests

--- queue/include/queue.hpp
+++ queue/include/queue.hpp
@@ -18,9 +18,13 @@ public:
     ~queue_list ();
     
     void push (const T& value);
+    void pop ();
 
-    T& front ();
-    const T& front () const;
+    T& front () const &;
+    T& back () const &;
+
+    size_t size () const;
+    bool empty () const;
 
 private:
     size_t size_ = 0;
@@ -31,41 +35,65 @@ private:
 template <typename T> 
 queue_list<T>::~queue_list ()
 {
-    q_node<T>* temp = rear_->next_;
+    q_node<T>* temp = front_->next_;
     for (int count = 0; count != size_ - 1; ++count)
     {
-        delete rear_;
-        rear_ = temp;
-        temp = rear_->next_;
+        delete front_;
+        front_ = temp;
+        temp = front_->next_;
     }
 
-    delete rear_;
+    delete front_;
 }
 
 template <typename T>
 void queue_list<T>::push (const T& value)
 {   
-    q_node<T>* node = new q_node<T> {value, rear_};
+    q_node<T>* node = new q_node<T> {value};
 
     if (rear_ == nullptr)
         rear_ = front_ = node;
 
+    rear_->next_ = node;
     rear_ = node;
     size_++;
 }
 
 template <typename T>
-T& queue_list<T>::front ()
+void queue_list<T>::pop ()
+{
+    auto new_front = front_->next_;
+
+    delete front_;
+    front_ = new_front;
+
+    size_--;
+}
+
+template <typename T>
+T& queue_list<T>::front () const &
 {
     if (size_)
         return front_->data_;
 }
 
-template <typename T> 
-const T& queue_list<T>::front () const
+template <typename T>
+T& queue_list<T>::back () const &
 {
     if (size_)
-        return front_->data_;
+        return rear_->data_;
+}       
+
+template <typename T>
+size_t queue_list<T>::size () const
+{
+    return size_;
+}
+
+template <typename T>
+bool queue_list<T>::empty () const
+{
+    return size_ == 0;
 }
 
 }   
--- queue/tests/tests.cpp
+++ queue/tests/tests.cpp
@@ -11,7 +11,7 @@ int main ()
     return RUN_ALL_TESTS ();
 }
 
-TEST (queue_list, consructor)
+TEST (queue, front)
 {
     queue_list<int> queue;
 
@@ -21,3 +21,50 @@ TEST (queue_list, consructor)
 
     ASSERT_EQ (queue.front (), 12);
 }
+
+TEST (queue, back)
+{
+    queue_list<int> queue;
+
+    queue.push (62);
+    queue.push (41);
+    queue.push (35);
+
+    ASSERT_EQ (queue.back (), 35);
+}
+
+TEST (queue, size_empty)
+{
+    queue_list<int> queue;
+
+    ASSERT_EQ (queue.empty (), true);
+    ASSERT_EQ (queue.size (), 0);
+    
+    queue.push (62);
+    queue.push (41);
+    ASSERT_EQ (queue.size (), 2);
+
+    queue.push (35);
+    ASSERT_EQ (queue.size (), 3);
+}
+
+TEST (queue, push_pop)
+{
+    queue_list<int> queue;
+
+    queue.push (1);
+    queue.push (2);
+    queue.push (3);
+
+    ASSERT_EQ (queue.back (), 3);
+    ASSERT_EQ (queue.front (), 1);
+
+    queue.pop ();
+    ASSERT_EQ (queue.back (), 3);
+    ASSERT_EQ (queue.front (), 2);
+    
+    queue.push (4);
+    ASSERT_EQ (queue.back (), 4);
+    ASSERT_EQ (queue.front (), 2);
+}
+
