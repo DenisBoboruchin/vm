78d637e3624b4574e909605ae13c429cbd237fe1
Denis <denis.boboruchin@gmail.com>
2022-11-27 12:40:23 +0300

    gueue_list done, add queue tests

--- queue/include/queue.hpp
+++ queue/include/queue.hpp
@@ -1,6 +1,9 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
 
+#include "list.hpp"
+#include "stack.hpp"
+
 namespace my_containers
 {
 
@@ -8,28 +11,89 @@ template <typename T>
 class Iqueue 
 {
 public:
-#if 0
-    virtual Iqueue () = 0;
-    virtual Iqueue (const Iqueue& other) = 0;
-    virtual Iqueue (Iqueue&& other) = 0;
-
-    virtual Iqueue& operator= (const Iqueue& other) = 0;
-    virtual Iqueue& operator= (Iqueue&& other) = 0;
-#endif
-
     virtual ~Iqueue() {};
 
-#if 1
-    virtual void push (T& value) = 0;
+    virtual void push (const T& value) = 0;
     virtual void pop () = 0;
 
-    virtual T& front () = 0;
-    virtual T& back () = 0;
 
-    virtual size_t size () = 0;
-    virtual bool empty () = 0;
-#endif
+    virtual T& front () & = 0;
+    virtual T& back () & = 0;
+
+    virtual const T& front () const & = 0;
+    virtual const T& back () const & = 0;
+
+    virtual size_t size () const = 0;
+    virtual bool empty () const = 0;
 };
+
+template <typename T>
+class queue_list final : public Iqueue<T> 
+{
+public:
+    void push (const T& value) override;
+    void pop () override;
+
+    T& front () & override;
+    T& back () & override;
+
+    const T& front () const & override;
+    const T& back () const & override;
+
+    size_t size () const override;
+    bool empty () const override;
+
+private:
+    list<T> list_ {}; 
+};
+
+template <typename T>
+void queue_list<T>::push (const T& value)
+{
+    list_.push_back (value);
+}
+
+template <typename T>
+void queue_list<T>::pop ()
+{
+    list_.pop_front ();
+}
+
+template <typename T>
+T& queue_list<T>::front () &
+{
+    return list_.front ();
+}
+
+template <typename T>
+const T& queue_list<T>::front () const &
+{
+    return list_.front ();
+}
+
+template <typename T>
+T& queue_list<T>::back () &
+{
+    return list_.back ();
+}
+
+template <typename T>
+const T& queue_list<T>::back () const &
+{
+    return list_.back ();
+}
+
+template <typename T>
+size_t queue_list<T>::size () const
+{
+    return list_.size ();
+}
+
+template <typename T>
+bool queue_list<T>::empty () const
+{
+    return list_.empty ();
+}
 
 }
 
--- queue/tests/tests.cpp
+++ queue/tests/tests.cpp
@@ -3,6 +3,7 @@
 #include "queue.hpp"
 
 using my_containers::Iqueue;
+using my_containers::queue_list;
 
 int main ()
 {
@@ -10,3 +11,150 @@ int main ()
 
     return RUN_ALL_TESTS();
 }
+
+TEST (queue, size_empty)
+{
+    queue_list<int> queue;
+    ASSERT_EQ (queue.empty (), true);
+
+    queue.push (13);
+    ASSERT_EQ (queue.size (), 1);
+    ASSERT_EQ (queue.front (), 13);
+    ASSERT_EQ (queue.back (), 13);   
+} 
+
+TEST (queue, back_front)
+{
+    queue_list<int> queue;
+
+    queue.push (1);
+    ASSERT_EQ (queue.front (), 1);
+    ASSERT_EQ (queue.back (), 1);   
+    
+    queue.push (2);
+    ASSERT_EQ (queue.front (), 1);
+    ASSERT_EQ (queue.back (), 2);    
+
+    queue.push (3);
+    ASSERT_EQ (queue.front (), 1);
+    ASSERT_EQ (queue.back (), 3);    
+}
+
+TEST (queue, push_pop)
+{
+    queue_list<int> queue;
+    
+    queue.pop ();
+
+    queue.push (1);
+    ASSERT_EQ (queue.size (), 1);
+    ASSERT_EQ (queue.front (), 1);
+    ASSERT_EQ (queue.back (), 1);   
+    
+    queue.push (2);
+    ASSERT_EQ (queue.front (), 1);
+    ASSERT_EQ (queue.back (), 2);    
+    
+    queue.push (3);
+
+    queue.pop ();
+    queue.pop ();
+    queue.pop ();
+
+    ASSERT_EQ (queue.size (), 0);
+    ASSERT_EQ (queue.empty (), true);
+
+    queue.push (20);
+    ASSERT_EQ (queue.front (), 20);
+    ASSERT_EQ (queue.back (), 20);     
+}
+
+TEST (queue, copy_constructor)
+{
+    queue_list<int> queue1;
+    
+    queue1.pop ();
+
+    queue1.push (20);
+    queue1.push (10);
+    ASSERT_EQ (queue1.size (), 2);
+    ASSERT_EQ (queue1.front (), 20);
+    ASSERT_EQ (queue1.back (), 10);   
+    
+    queue_list<int> queue2 {queue1};
+
+    ASSERT_EQ (queue2.size (), 2);
+    ASSERT_EQ (queue2.front (), 20);
+    ASSERT_EQ (queue2.back (), 10);   
+}
+
+TEST (queue, move_constructor)
+{
+    queue_list<int> queue1;
+    
+    queue1.pop ();
+
+    queue1.push (20);
+    queue1.push (10);
+    ASSERT_EQ (queue1.size (), 2);
+    ASSERT_EQ (queue1.front (), 20);
+    ASSERT_EQ (queue1.back (), 10);   
+    
+    queue_list<int> queue2 {std::move(queue1)};
+
+    ASSERT_EQ (queue2.size (), 2);
+    ASSERT_EQ (queue2.front (), 20);
+    ASSERT_EQ (queue2.back (), 10);   
+}
+
+TEST (queue, copy_assignment)
+{
+    queue_list<int> queue1;
+    
+    queue1.pop ();
+
+    queue1.push (20);
+    queue1.push (10);
+    ASSERT_EQ (queue1.size (), 2);
+    ASSERT_EQ (queue1.front (), 20);
+    ASSERT_EQ (queue1.back (), 10);   
+    
+    queue_list<int> queue2 {};
+    queue2.push (1221);
+
+    queue2 = queue1;
+
+    ASSERT_EQ (queue2.size (), 2);
+    ASSERT_EQ (queue2.front (), 20);
+    ASSERT_EQ (queue2.back (), 10);   
+}
+
+TEST (queue, move_assignment)
+{
+    queue_list<int> queue1;
+    
+    queue1.pop ();
+
+    queue1.push (20);
+    queue1.push (10);
+    ASSERT_EQ (queue1.size (), 2);
+    ASSERT_EQ (queue1.front (), 20);
+    ASSERT_EQ (queue1.back (), 10);   
+    
+    queue_list<int> queue2 {};
+    queue2.push (111);
+    
+    queue2 = std::move(queue1);
+
+    ASSERT_EQ (queue2.size (), 2);
+    ASSERT_EQ (queue2.front (), 20);
+    ASSERT_EQ (queue2.back (), 10);   
+}
+
+TEST (queue, virtual_destructor)
+{
+    Iqueue<int>* base_ptr = new queue_list<int> {};
+
+    delete base_ptr;
+}
+
