91e613c7a326399d2e74217a9c7ed1efb2e389c3
Denis <denis.boboruchin@gmail.com>
2022-12-14 16:44:23 +0300

    added artificial bench for list win

--- queue/benchmark/benchmark.cpp
+++ queue/benchmark/benchmark.cpp
@@ -27,20 +27,34 @@ static void BM_queue_native(benchmark::State &state)
 BENCHMARK(BM_queue_native<my_containers::queue_stacks<int>>)->Arg(RANGE_QUEUE_NATIVE)->Unit(benchmark::kMillisecond);
 BENCHMARK(BM_queue_native<my_containers::queue_list<int>>)->Arg(RANGE_QUEUE_NATIVE)->Unit(benchmark::kMillisecond);
 
+struct my_bench_class final
+{
+    my_bench_class () noexcept {};
+
+    my_bench_class& operator= (const my_bench_class& other)
+    {    
+        for (int i = 0; i != 10000; i++)
+        {}
+
+        return *this;
+    };
+};
+
 template <typename queue_type>
-static void BM_queue_hard(benchmark::State &state)
+static void BM_queue_artificial(benchmark::State &state)
 {
     for (auto _ : state) {
         queue_type queue;
 
         for (int count = 0; count != state.range(0); ++count) {
-            queue.push(count);
+            my_bench_class bench_class {};
+            queue.push(bench_class);
             queue.pop();
         }
     }
 }
 
-BENCHMARK(BM_queue_hard<my_containers::queue_stacks<int>>)->Arg(RANGE_QUEUE_HARD)->Unit(benchmark::kMillisecond);
-BENCHMARK(BM_queue_hard<my_containers::queue_list<int>>)->Arg(RANGE_QUEUE_HARD)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_queue_artificial<my_containers::queue_stacks<my_bench_class>>)->Arg(RANGE_QUEUE_HARD)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_queue_artificial<my_containers::queue_list<my_bench_class>>)->Arg(RANGE_QUEUE_HARD)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
