51948ea60dfdf2f9726eb14790afaf856481e056
Denis <denis.boboruchin@gmail.com>
2023-02-25 22:20:08 +0300

    add hash_insert, create tests

--- teachable_dictionary/hash_table/include/hash_table.hpp
+++ teachable_dictionary/hash_table/include/hash_table.hpp
@@ -3,11 +3,73 @@
 
 #include <iostream>
 
+#include "list.hpp"
+
 namespace my_containers {
 
-template <typename T>
+template <typename Key, typename T, typename Hash = std::hash<Key>>
 class hash_table final {
+public:
+    hash_table () {};
+    hash_table (const hash_table& other) = default;
+    hash_table (hash_table&& other) noexcept = default;
+
+    hash_table& operator= (const hash_table& other) = default;
+    hash_table& operator= (hash_table&& other) noexcept = default;
+
+    bool insert (const Key& key, const T& elem);
+    T* find (const Key& key) const;
+
+    size_t size () const;     
+    bool empty () const;
+
+private:
+    static constexpr size_t NUM_HASH_BUCKETS = 1024;
+    using Pair_key_elem = typename std::pair<Key, T>;
+
+    size_t size_ = 0;
+    std::array<list<Pair_key_elem>, NUM_HASH_BUCKETS> data_ {}; 
 };
+
+template <typename Key, typename T, typename Hash>
+bool hash_table<Key, T, Hash>::insert (const Key& key, const T& elem) 
+{
+    int index = Hash {} (key) / NUM_HASH_BUCKETS;
+    list<Pair_key_elem>& list_of_pairs = data_.at(index);
+    
+    Pair_key_elem new_pair = {key, elem};
+    
+    if (!list_of_pairs.empty ())
+    {
+        Pair_key_elem candidate = list_of_pairs.find (new_pair);
+    
+        if (candidate == new_pair)
+            return 0;
+    }
+
+    list_of_pairs.push_front (new_pair);
+    size_++;
+
+    return 1;
+}
+
+template <typename Key, typename T, typename Hash>
+T* hash_table<Key, T, Hash>::find (const Key& key) const
+{
+    //if (Hash {} (key))
+}
+
+template <typename Key, typename T, typename Hash>
+size_t hash_table<Key, T, Hash>::size () const
+{
+    return size_;
+}
+
+template <typename Key, typename T, typename Hash>
+bool hash_table<Key, T, Hash>::empty () const
+{
+    return size_ == 0;
+}
 
 }  // namespace my_containers
 
--- teachable_dictionary/hash_table/tests/tests.cpp
+++ teachable_dictionary/hash_table/tests/tests.cpp
@@ -2,9 +2,31 @@
 
 #include "hash_table.hpp"
 
+using my_containers::hash_table;
+
 int main()
 {
     testing::InitGoogleTest();
 
     return RUN_ALL_TESTS();
 }
+
+TEST (hash_table, constructors)
+{
+    hash_table<int, int> first_table;
+   
+    ASSERT_EQ (first_table.size (), 0);
+    ASSERT_EQ (first_table.empty (), 1);
+}
+
+TEST (hash_table, insert)
+{
+    hash_table<int, int> first_table;
+   
+    ASSERT_EQ (first_table.size (), 0);
+    ASSERT_EQ (first_table.empty (), 1);
+
+    ASSERT_EQ (first_table.insert (1, 3), 1);
+    ASSERT_EQ (first_table.size (), 1);
+}
+
