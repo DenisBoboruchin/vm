58f36efbc97cd781faa566429582fe02bb939976
Denis <denis.boboruchin@gmail.com>
2023-02-26 14:17:16 +0300

    add reader folder

--- teachable_dictionary/hash_table/include/hash_table.hpp
+++ teachable_dictionary/hash_table/include/hash_table.hpp
@@ -10,7 +10,7 @@ namespace my_containers {
 template <typename Key, typename T, typename Hash = std::hash<Key>>
 class hash_table final {
 public:
-    hash_table () {};
+    hash_table () = default;
     hash_table (const hash_table& other) = default;
     hash_table (hash_table&& other) noexcept = default;
 
@@ -28,7 +28,8 @@ private:
     using Pair_key_elem = typename std::pair<Key, T>;
 
     size_t size_ = 0;
-    std::array<list<Pair_key_elem>, NUM_HASH_BUCKETS> data_ {}; 
+    int last_insert_index = -1;
+    std::array<list<Pair_key_elem>, NUM_HASH_BUCKETS> data_ {};
 };
 
 /*
@@ -53,22 +54,25 @@ bool hash_table<Key, T, Hash>::insert (const Key& key, const T& elem)
 
     return 1;
 }
+*/
 
+/*
 template <typename Key, typename T, typename Hash>
-bool hash_table<Key, T, Hash>::find (const Key& key) const
+T& hash_table<Key, T, Hash>::find (const Key& key) const
 {
     int index = Hash {} (key) % NUM_HASH_BUCKETS;
     list<Pair_key_elem>& list_of_pairs = data_.at(index);
     
     Pair_key_elem new_pair = {key, elem};
  
-    if (list_of_pairs.find (new_pair))
+    if (!list_of_pairs.empty ())
     {
-        return 1;
+        if (lists_of_pairs.get_elem (new_pair) 
     }
-   
-    return 0;
-}*/
+  
+    return data_.at(last_insert_index).front ();
+}
+*/
 
 template <typename Key, typename T, typename Hash>
 size_t hash_table<Key, T, Hash>::size () const
---
+++ teachable_dictionary/reader/tests/tests.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include "reader.hpp"
+
+int main ()
+{
+    testing::InitGoogleTest();
+
+    return RUN_ALL_TESTS ();
+}
