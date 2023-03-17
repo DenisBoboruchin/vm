a0dfff0681bd5646904f0958a25f6f1d85eab71c
Denis <denis.boboruchin@gmail.com>
2023-03-18 01:18:10 +0300

    started dictionary manager

--- teachable_dictionary/src/main.cpp
+++ teachable_dictionary/src/main.cpp
@@ -2,4 +2,44 @@
 
 #include "teachable_dictionary.hpp"
 
-int main(int argc, char **argv) {}
+static int execute ();
+static int command_handler (const std::string& command);
+
+int main(int argc, char **argv) {
+    std::cout << "teachable dictionary manager executing" << std::endl;
+    std::cout << "input command (h -- for help, q -- for exit)" << std::endl;
+
+    return execute ();
+}
+
+int execute ()
+{
+    std::string command {};
+
+    std::cin >> command;
+
+    while ((command != "q") && (command != "quit"))
+    {
+        command_handler (command);
+
+        std::cin >> command;
+    }
+
+    return 1;
+}   
+
+int command_handler (const std::string& command)
+{
+    if (command == "help")
+    {
+        std::cout << "dictionary manager help menu" << std::endl;
+    }
+
+    else
+    {
+        std::cout << "unknown command \"" << command << "\", please try again" << std::endl;
+    }
+
+    return 1;
+}
+
