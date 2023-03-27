a699a632bd715a27cae643f83b36bc36af78ab95
Denis <denis.boboruchin@gmail.com>
2023-03-27 16:07:25 +0300

    implemented list::emplace

--- queue/build/CMakeFiles/STACK_LIB.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libSTACK_LIB.a"
-)
--- queue/build/CMakeFiles/clang_format.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clang_format"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clang_format.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
